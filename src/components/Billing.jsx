188f0c5e39c97ae97afcdfdeacc10d3465804ee3
BlakeZajac <contact@blakezajac.com>
2022-10-25 22:57:35 +1100

    Completed website files

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,48 @@ import { apple, bill, google } from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => {
-  return <section id="product" className={layout.sectionReverse}></section>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing industries for previewing layouts and visual mockups.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6 gap-4">
+          <div className="bg-blue-gradient w-fit h-fit rounded-lg p-[1px] hover:opacity-80">
+            <img
+              src={apple}
+              alt="app_store"
+              className="w-fit h-fit object-contain cursor-pointer"
+            />
+          </div>
+          <div className="bg-blue-gradient w-fit h-fit rounded-lg p-[1px] hover:opacity-80">
+            <img
+              src={google}
+              alt="google_play"
+              className="w-fit h-fit object-contain cursor-pointer"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -31,13 +31,13 @@ const Business = () => (
         You do the business, <br className="sm:block hidden" /> we’ll handle the
         money.
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 mb-10`}>
         With the right credit card, you can improve your financial life by
         building credit, earning rewards and saving money. But with hundreds of
         credit cards on the market.
       </p>
 
-      <Button styles={`mt-10`} />
+      <Button />
     </div>
 
     <div className={`${layout.sectionImg} flex-col`}>
